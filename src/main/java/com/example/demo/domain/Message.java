e2241b6e4c33ce4ac427446381f757852aa4054f
inseok9068 <dlstr9068@gmail.com>
2020-04-25 19:52:39 +0900

    2020-04-25 이인석

---
+++ src/main/java/com/example/demo/domain/Message.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain;
+
+public class Message {
+	private String Memo;
+
+	public String getMemo() {
+		return Memo;
+	}
+
+	public void setMemo(String memo) {
+		Memo = memo;
+	}
+}
--- src/main/java/com/example/demo/web/TestController.java
+++ src/main/java/com/example/demo/web/TestController.java
@@ -1,7 +1,12 @@
 package com.example.demo.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Message;
 
 @RestController
 public class TestController {
@@ -15,5 +20,16 @@ public class TestController {
     public String demo_test() throws Exception{
         return "Hello demo(/demo)";
     }
+    
+    @PostMapping("/message")
+    @ResponseBody
+    public String pushMessage(@RequestBody Message msg) throws Exception{
+
+	    if(msg.getMemo() != null || msg.getMemo().equals("")) {
+	    	throw new Exception("메모를 입력해주세요.");
+	    }
+	    
+	    return "성공";
+	}
  
 }
