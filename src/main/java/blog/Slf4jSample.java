2f4285afe97dd483c925810fe5235143d3e9c36e
dlstjr9068 <dlstjr9068@gmail.com>
2020-06-16 15:44:57 +0900

    2020-06-16 Lombok Annotation @Slf4j 정리

---
+++ src/main/java/blog/Slf4jSample.java
@@ -0,0 +1,19 @@
+package blog;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Slf4jSample {
+	
+	public static void main(String[] args) {
+		
+		String value1 = "1번값";
+		String value2 = "2번값";
+		
+		if(log.isDebugEnabled()){
+			log.info("---------- Log 테스트 ---------");
+			log.info("1번의 값은 : "+value1 + " 2번의 값은 : "+value2);
+			log.info("1번의 값은 : {} 2번의 값은 : {}", value1, value2);
+		}
+	}
+}
