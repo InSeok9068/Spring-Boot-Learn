98ee36d8d9d63e2c2d2a4d9b76103ec8a9ee3135
dlstjr9068 <dlstjr9068@gmail.com>
2021-02-04 18:24:06 +0900

    JSP 제거 및 Thymeleaf Template 엔진 변경

--- src/main/java/com/example/demo/config/web/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/web/WebMvcConfig.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesView;
-import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @RequiredArgsConstructor
 @Configuration
