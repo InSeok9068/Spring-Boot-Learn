e3892378af3db51db31efe63a33c48c7bd2a8442
dlstjr9068 <dlstjr9068@gmail.com>
2021-03-19 16:19:13 +0900

    Response ObjectMapper 객체 Config 설정 추가

--- src/main/java/com/example/demo/config/web/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/web/WebMvcConfig.java
@@ -3,18 +3,16 @@ package com.example.demo.config.web;
 import com.example.demo.config.filter.CustomFilter;
 import com.example.demo.config.interceptor.Interceptor;
 import com.example.demo.config.xss.HTMLCharacterEscapes;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -40,6 +38,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Bean
 	public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
 		ObjectMapper objectMapper = new ObjectMapper();
+		// ObjectMapper Config 설정
+		// TODO MappingJackson2HttpMessageConverter 해당 클래스의 Default ObjectMapper를 사용하는 방법을 찾아보면 좋을듯 함.
+		objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,false);
+		objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION,true);
 		objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
 		return new MappingJackson2HttpMessageConverter(objectMapper);
 	}
